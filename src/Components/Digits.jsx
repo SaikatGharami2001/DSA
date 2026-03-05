f5991688550d61f476f9d76d3eba3a93a88c6e91
SaikatGharami2001 <saikatgharami2@gmail.com>
2026-03-05 20:36:46 +0530

    Digit Counter

--- src/Components/Digits.jsx
+++ src/Components/Digits.jsx
@@ -1,12 +1,21 @@
 const Digit = () => {
-  //
-  let a = 10;
-  let c = 0;
-  while (a > 0) {
-    a = Math.floor(a / 10);
-    c++;
+  // Write a function that returns the count of digits in a number
+  const number = -123456789;
+  let count = 0;
+  function digitCounter(num) {
+    if (num < 0) num = Math.abs(num);
+    if (num == 0) return 1;
+
+    let newNum = num;
+    while (newNum > 0) {
+      newNum = Math.floor(newNum / 10);
+      count++;
+    }
+    return count;
   }
-  console.log(c);
+
+  const result = digitCounter(number);
+  console.log(result);
 
   return <div>Digit</div>;
 };
